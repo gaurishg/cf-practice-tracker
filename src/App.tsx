275fc2e2dabeffcd05c86bdf4b4e8a8b51b24f7d
gaurishg <gaurishgangwar@gmail.com>
2023-01-12 10:55:51 +0000

    Reorganized Themeprovider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Stack } from "@mui/material";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { MyThemeProvider } from "./component/MyThemeProvider";
 import { TopBar } from "./component/TopBar";
 import { A2OJLadders } from "./pages/A2OJLadders";
 import { A2OJTablePage } from "./pages/A2OJTablePage";
@@ -8,22 +7,20 @@ import { A2OJTablePage } from "./pages/A2OJTablePage";
 function App(): JSX.Element {
   console.log("App rendered again");
   return (
-    <MyThemeProvider>
-      <center>
-        <Stack maxWidth={800}>
-          <TopBar />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Navigate to="/a2oj" replace />} />
-              <Route path="/a2oj" element={<A2OJLadders />}>
-                <Route path=":problemset_id" element={<A2OJTablePage />} />
-              </Route>
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </BrowserRouter>
-        </Stack>
-      </center>
-    </MyThemeProvider>
+    <center>
+      <Stack maxWidth={800}>
+        <TopBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/a2oj" replace />} />
+            <Route path="/a2oj" element={<A2OJLadders />}>
+              <Route path=":problemset_id" element={<A2OJTablePage />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </Stack>
+    </center>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { MyThemeProvider } from "./component/MyThemeProvider";
 import "./index.scss";
 import { AppContextProvider } from "./store/app-context";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <AppContextProvider>
-      <App />
+      <MyThemeProvider>
+        <App />
+      </MyThemeProvider>
     </AppContextProvider>
   </React.StrictMode>
 );
